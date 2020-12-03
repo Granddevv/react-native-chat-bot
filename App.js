f1d65117e00e3b1ba509a794addd37f0971afea0
granddevv <spacingflight@outlook.com>
2020-12-03 21:54:36 +0800

    initial commits

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatMainScreen from "./src/screens/chat-main/chat-main";
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <ChatMainScreen />
+    </>
+  );
+};
+
+export default App;
